d1b569e26c0164d52b8330b0c83293b63590d3bc
Marina Sysoliatina <stereogam@yandex.ru>
2026-01-19 17:32:03 +0100

    refactor store code

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,19 +1,30 @@
-import { create } from 'zustand'
+import { create, type StateCreator } from 'zustand'
+interface IInintialState {
+     bodies: number,
+}
 
-interface IUseBodiesCounterStore {
-    bodies: number,
+interface IActions {
     likeBody: () => void,
     unlikeBody: () => void,
 }
 
-export const useBodiesCounterStore = create<IUseBodiesCounterStore>()((set) => ({
-    bodies: 0,
+interface IUseBodiesCounterStore extends IInintialState, IActions{}
+
+const initialState : IInintialState = {
+     bodies: 0
+}
+
+const BodiesCounterStore : StateCreator<IUseBodiesCounterStore>= ((set) => ({
+   ...initialState,
     likeBody: () => set((state) => ({bodies: state.bodies + 1})),
     unlikeBody: () => set((state) => ({bodies: state.bodies - 1})),
     })
 )
 
+const useBodiesCounterStore = create<IUseBodiesCounterStore>()(BodiesCounterStore)
 
+
+//selectors
 export const useBodies = () => useBodiesCounterStore((state) => state.bodies)
 export const useLikeBody = () => useBodiesCounterStore.getState().likeBody
 export const useUnlikeBody = () => useBodiesCounterStore.getState().unlikeBody
