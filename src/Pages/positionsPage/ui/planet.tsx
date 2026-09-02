0d9b3a64d941798bd7616e1e826a01028a6a1906
Marina Sysoliatina <stereogam@yandex.ru>
2026-09-02 19:01:47 +0200

    extended sun system render

--- src/Pages/positionsPage/ui/planet.tsx
+++ src/Pages/positionsPage/ui/planet.tsx
@@ -1,31 +1,54 @@
 import { Text } from '@react-three/drei'
 import { stringToDecimal, getPositionFromAzAlt } from '../../../Shared/utils/stringToDecimal'
+import { PLANET_COLORS } from '../../../Shared/configs/meshColorPlanetMaterial/planetColorMaterial'
+import { useState, useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
+import * as THREE from 'three'
 
-type PlanetProps = {
+
+export type PlanetProps = {
     name: string
     az: string
     alt: string
 }
 
 export const Planet = ({name, az, alt} : PlanetProps) => {
+    const [hovered, setHovered] = useState(false)
+
+    const meshRef = useRef<THREE.Mesh>(null)
+
+    useFrame(() => {
+        if(!meshRef.current) 
+            return
+                const targetScale = hovered ? 1.4 : 1
+                meshRef.current.scale.x += (targetScale - meshRef.current.scale.x) * 0.1
+                meshRef.current.scale.y += (targetScale - meshRef.current.scale.y) * 0.1
+                meshRef.current.scale.z += (targetScale - meshRef.current.scale.z) * 0.1     
+    })
 
     const azDecimal = stringToDecimal(az)
     const altDecimal = stringToDecimal(alt)
 
     const position = getPositionFromAzAlt(azDecimal, altDecimal)
 
+    
+
 
     return (
         <group position={position}>
-            <mesh>
-                <sphereGeometry args={[0.3, 32, 32]}/>
-                <meshStandardMaterial color="#f9e6da"/>
+            <mesh ref={meshRef} onPointerOver={() => setHovered(true)} onPointerOut={() => setHovered(false)} >
+                <sphereGeometry  args={[0.3, 32, 32]}/>
+                <meshStandardMaterial 
+                color={PLANET_COLORS[name]}
+                roughness={1}
+                />
             </mesh>    
             <Text
             anchorX="left"
             anchorY="middle"
-            fontSize={0.3}
+            fontSize={0.2}
             position={[0.5, 0, 0]}
+            fillOpacity={0.6}
             >{name}</Text>
         </group>
     )
--- src/Pages/positionsPage/ui/positions.tsx
+++ src/Pages/positionsPage/ui/positions.tsx
@@ -6,7 +6,9 @@ import { useCustomPositionBodiesStore } from '../../../stores/useCustomPositionB
 import customBodies from '../../../Shared/api/mockApiData/customPositionBodies.json'
 import { Canvas } from '@react-three/fiber'
 import { Planet } from './planet'
-import { OrbitControls } from '@react-three/drei'
+import { Soleil } from './soleil'
+import { OrbitControls, Cloud, Sparkles, Text} from '@react-three/drei'
+
 
 export type Position = {
       name: string,
@@ -45,13 +47,13 @@ export type PositionApiResponse = {
 export type Positions = Position[]
 
 export const Positions = () => {
-    const [searchParams, setSearchParams] = useSearchParams()
+const [searchParams, setSearchParams] = useSearchParams()
 
   const fetchBodies = useCustomPositionBodiesStore((store) => store.getCustomBodies);
 //   const bodyList = useCustomPositionBodiesStore((store) => store.bodies); 
   const loading = useCustomPositionBodiesStore((store) => store.loading);
 
-    const initialValue = {
+const initialValue = {
         lat: searchParams.get('lat') ?? '',
         lon: searchParams.get('lon') ?? '',
         elev: searchParams.get('elev') ?? '',
@@ -59,9 +61,9 @@ export const Positions = () => {
         zone: searchParams.get('zone') ?? '',
     }
 
-const handleSubmit = () => {
-    fetchBodies(initialValue)
-}
+    const handleSubmit = () => {
+        fetchBodies(initialValue)
+        }
 
     return (
         <div className={styles.page}>
@@ -82,15 +84,70 @@ const handleSubmit = () => {
             })} */}
                  <div className={styles.scene}>
                     <Canvas camera={{ position: [0, 2, 10] }}>
-                        <color attach="background" args={['#040414']} />
-                      <OrbitControls minDistance={5} maxDistance={25} enablePan={false}/>
-                      <ambientLight intensity={0.2} />
-                      <pointLight position={[5, 5, 5]} intensity={100} />
+                        <color attach="background" args={['#000000']} />
+                        <mesh position={[0, 5, 0]}>
+                            <coneGeometry 
+                            args={[0.08, 0.25, 10]}
+                            />
+                            <meshStandardMaterial color="#ffffff" />
+                        </mesh>
+                        <Text
+                    position={[0, 5.3, 0]}
+                    color='#454545'
+                    anchorX="center"
+                    anchorY="middle"
+                    fontSize={0.25}
+                    >Zenith</Text>
+                    <mesh rotation={[-Math.PI / 2, 0, 0]}>
+                        <ringGeometry 
+                        args={[4.95, 5, 80]}
+                        />
+                        <meshBasicMaterial  
+                        transparent
+                        opacity={0.15}
+                        color="#b0c0ff"
+                        />
+                    </mesh>
+                        <Text position={[0, 0, -5]} color='#4b4b6e' fillOpacity={0.4} anchorX="center" anchorY="middle">N</Text>
+                        <Text position={[5, 0, 0]} color='#4b666e' fillOpacity={0.4} anchorX="center" anchorY="middle">E</Text>
+                        <Text position={[0, 0, 5]} color='#6e574b' fillOpacity={0.4} anchorX="center" anchorY="middle">S</Text>
+                        <Text position={[-5, 0, 0]} color='#5e6e4b' fillOpacity={0.4} anchorX="center" anchorY="middle">W</Text>
+                      <OrbitControls 
+                        minDistance={5} 
+                        maxDistance={15} 
+                        enablePan={false}/>
+                      <ambientLight 
+                        intensity={0.5} />
+                      <pointLight 
+                        position={[5, 0, 5]} 
+                        intensity={100} 
+                        color='#ffe7a3'/>
                         {customBodies.positions?.map((body) => {
                             return (
-                             <Planet key={body.name} name={body.name} alt={body.alt} az={body.az} />
-                            )
-                        })}
+                                
+                                body.name === 'Soleil'? <Soleil 
+                                key={body.name} 
+                                name={body.name} 
+                                alt={body.alt} 
+                                az={body.az}/> : 
+                                    <Planet 
+                                    key={body.name} 
+                                    name={body.name} 
+                                    alt={body.alt} 
+                                    az={body.az} />
+                                )
+                            }  
+                        )}
+                        <Sparkles 
+                        color='#fad98b'
+                        size={3}
+                        opacity={0.95}
+                        count={200}
+                        speed={0.2}
+                        scale={15}
+                        noise={20}
+                        />
+                        <Cloud seed={10} scale={2} volume={10} color="#7777b4" fade={200} />
                     </Canvas>
                 </div>
         </div>
